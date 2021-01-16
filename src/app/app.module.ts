569c15d745b4a4f4e0008a0ea3108ddfcdcfc58e
Miguel <miguelmoreno@innovatiod.tech>
2021-01-15 21:04:51 -0500

    Configuración de componentes base y rutas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginSigninComponent } from './components/login/login-signin/login-signin.component';
+import { LoginSignupComponent } from './components/login/login-signup/login-signup.component';
+import { LoginCodeComponent } from './components/login/login-code/login-code.component';
+import { LoginComponent } from './view/login/login.component';
+import { DashboardComponent } from './view/dashboard/dashboard.component';
+
+// Definición de rutas
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login', pathMatch: 'full' },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginSigninComponent,
+    LoginSignupComponent,
+    LoginCodeComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/login/login-code/login-code.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-code',
+  templateUrl: './login-code.component.html',
+  styleUrls: ['./login-code.component.css']
+})
+export class LoginCodeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
