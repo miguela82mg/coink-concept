f34258f69b1c8390f21d312eead530bf7a5c049d
Miguel <miguelmoreno@innovatiod.tech>
2021-01-17 23:12:15 -0500

    Cambios en los estilos de la tabla

--- src/app/components/dashboard/dashboard-datatable/dashboard-datatable.component.ts
+++ src/app/components/dashboard/dashboard-datatable/dashboard-datatable.component.ts
@@ -9,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardDatatableComponent implements OnInit {
 
   // Cabecera
-  header: string[] = ['Nombre', 'Cel', 'Email', 'Edad', 'Fecha de comprar']
+  header: string[] = ['Nombre', 'Celular', 'Email', 'Edad', 'Fecha de comprar']
   // Contenido de la tabla local
   localTransactions: Array<any> = [{ name: 'Carlos Gonzales', cel: '310 345 4356', email: 'cgonza@gmail.com', date: '11/03/2020', age: 24 }, { name: 'Pumarejo', cel: '310 345 4356', email: 'cgonza@gmail.com', date: '11/03/2020', age: 24 }, { name: 'Pumarejo', cel: '310 345 4356', email: 'cgonza@gmail.com', date: '11/03/2020', age: 24 }, { name: 'Pumarejo', cel: '310 345 4356', email: 'cgonza@gmail.com', date: '11/03/2020', age: 24 }, { name: 'Pumarejo', cel: '310 345 4356', email: 'cgonza@gmail.com', date: '11/03/2020', age: 24 }, { name: 'Carlos Gonzales', cel: '310 345 4356', email: 'cgonza@gmail.com', date: '11/03/2020', age: 24 }, { name: 'Pumarejo', cel: '310 345 4356', email: 'cgonza@gmail.com', date: '11/03/2020', age: 24 }, { name: 'Pumarejo', cel: '310 345 4356', email: 'cgonza@gmail.com', date: '11/03/2020', age: 24 }, { name: 'Pumarejo', cel: '310 345 4356', email: 'cgonza@gmail.com', date: '11/03/2020', age: 24 }, { name: 'Pumarejo', cel: '310 345 4356', email: 'cgonza@gmail.com', date: '11/03/2020', age: 24 },]
 
